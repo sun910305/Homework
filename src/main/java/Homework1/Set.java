f625cbd3a46578b98d63ea881d79c238da471d4b
sunwei <sun910305@gmail.com>
2021-03-11 17:27:16 +0800

    homework customer

--- src/main/java/Homework1/Set.java
+++ src/main/java/Homework1/Set.java
@@ -2,6 +2,7 @@ package Homework1;
 
 public class Set {
     public static void main(String[] args) {
+        System.out.println("客戶編號/消費金額/折讓/金額");
         Homework[] homework = {
                 new Homework ("0013" , 490,  0),
                 new Homework("0052", 1000,100),
